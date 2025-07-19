5ac326ceda09138b872fe2893f0409d7cc17c06b
Xunn <164025573+ItzXunn@users.noreply.github.com>
2025-07-19 17:41:59 +0700

    Update Locket_fix.js

--- Locket_fix.js
+++ Locket_fix.js
@@ -25,7 +25,7 @@ if (!obj.subscriber.entitlements) obj.subscriber.entitlements = {};
 if (!obj.subscriber.subscriptions) obj.subscriber.subscriptions = {};
 
 // ========= Tạo thông tin gói Locket Gold ========= //
-var hoangvanbao = {
+var xunn = {
   is_sandbox: false,
   ownership_type: "PURCHASED",
   billing_issues_detected_at: null,
@@ -38,10 +38,10 @@ var hoangvanbao = {
   store: "app_store"
 };
 
-var hvb_entitlement = {
+var xunn_entitlement = {
   grace_period_expires_date: null,
   purchase_date: specificDate, // Ngày tham gia
-  product_identifier: "com.hoangvanbao.premium.yearly",
+  product_identifier: "com.xunn.premium.yearly",
   expires_date: "2099-12-18T01:04:17Z" // Ngày hết hạn lâu dài
 };
 
@@ -50,18 +50,18 @@ const match = Object.keys(mapping).find(e => ua.includes(e));
 
 if (match) {
   let entitlementKey = mapping[match][0] || "Locket";
-  let subscriptionKey = mapping[match][1] || "com.hoangvanbao.premium.yearly";
+  let subscriptionKey = mapping[match][1] || "com.xunn.premium.yearly";
 
-  obj.subscriber.subscriptions[subscriptionKey] = hoangvanbao;
-  obj.subscriber.entitlements[entitlementKey] = hvb_entitlement;
+  obj.subscriber.subscriptions[subscriptionKey] = xunn;
+  obj.subscriber.entitlements[entitlementKey] = xunn_entitlement;
 } else {
   // Gán mặc định nếu không có khớp
-  obj.subscriber.subscriptions["com.hoangvanbao.premium.yearly"] = hoangvanbao;
-  obj.subscriber.entitlements["Locket"] = hvb_entitlement;
+  obj.subscriber.subscriptions["com.hoangvanbao.premium.yearly"] = xunn;
+  obj.subscriber.entitlements["Locket"] = xunn_entitlement;
 }
 
 // ========= Thêm thông báo và Log ========= //
-obj.Attention = "Chúc mừng bạn Xunn ^^ Vui lòng không bán hoặc chia sẻ cho người khác!";
+obj.Attention = "Chúc mừng bạn Xunn! Vui lòng không bán hoặc chia sẻ cho người khác!";
 console.log("User-Agent:", ua);
 console.log("Final Modified Response:", JSON.stringify(obj, null, 2));
 
